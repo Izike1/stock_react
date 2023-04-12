989a9ea168448c3a8005ee2558abd221e0656e47
Izikel <maxim.larionenko@gmail.com>
2023-04-12 15:36:58 +0300

    update project

--- server/index.js
+++ server/index.js
@@ -1,2 +1,14 @@
+require('dotenv');
+const express = require('express');
 
+const PORT = process.env.PORT || 5000;
 
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Let\'s go');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+})
