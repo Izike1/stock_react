5af5ffd9e2cab8e79f1faf5c38507ce3af02ed9b
Izikel <maxim.larionenko@gmail.com>
2023-04-13 09:12:25 +0300

    update client

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import Paper from '@mui/material/Paper';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import HomeIcon from '@mui/icons-material/Home';
@@ -20,48 +21,50 @@ export default function Header() {
     return (
         <>
             <header className={styles.header}>
-                <div className={styles.headerBody}>
-                    <div className={styles.headerContent}>
-                        <div className={styles.headerLogo}>
-                            <Link to="/home">
-                                <HomeIcon color="primary" fontSize="large" />
-                            </Link>
+                <Paper variant='outlined'>
+                    <div className={styles.headerBody}>
+                        <div className={styles.headerContent}>
+                            <div className={styles.headerLogo}>
+                                <Link to="/home">
+                                    <HomeIcon color="primary" fontSize="large" />
+                                </Link>
+                            </div>
+                            <nav className={styles.headerNav}>
+                                <Button
+                                    id="basic-button"
+                                    aria-controls={open ? 'basic-menu' : undefined}
+                                    aria-haspopup="true"
+                                    aria-expanded={open ? 'true' : undefined}
+                                    variant='text'
+                                    onClick={handleClick}
+                                >
+                                    Menu
+                                </Button>
+                                <Menu
+                                    id="basic-menu"
+                                    anchorEl={anchorEl}
+                                    open={open}
+                                    onClose={handleClose}
+                                    MenuListProps={{
+                                        'aria-labelledby': 'basic-button',
+                                    }}
+                                >
+                                    <MenuItem onClick={handleClose}>
+                                        <Link className={styles.headerLink} to="/staff">Staff</Link>
+                                    </MenuItem>
+                                    <MenuItem onClick={handleClose}>
+                                        <Link className={styles.headerLink} to="/stock">
+                                            Stock
+                                        </Link>
+                                    </MenuItem>
+                                    <MenuItem onClick={handleClose}>
+                                        <Link className={styles.headerLink} to='/suppliers'>Suppliers</Link>
+                                    </MenuItem>
+                                </Menu>
+                            </nav>
                         </div>
-                        <nav className={styles.headerNav}>
-                            <Button
-                                id="basic-button"
-                                aria-controls={open ? 'basic-menu' : undefined}
-                                aria-haspopup="true"
-                                aria-expanded={open ? 'true' : undefined}
-                                variant='text'
-                                onClick={handleClick}
-                            >
-                                Menu
-                            </Button>
-                            <Menu
-                                id="basic-menu"
-                                anchorEl={anchorEl}
-                                open={open}
-                                onClose={handleClose}
-                                MenuListProps={{
-                                    'aria-labelledby': 'basic-button',
-                                }}
-                            >
-                                <MenuItem onClick={handleClose}>
-                                    <Link className={styles.headerLink} to="/staff">Staff</Link>
-                                </MenuItem>
-                                <MenuItem onClick={handleClose}>
-                                    <Link className={styles.headerLink} to="/stock">
-                                        Stock
-                                    </Link>
-                                </MenuItem>
-                                <MenuItem onClick={handleClose}>
-                                    <Link className={styles.headerLink} to='/suppliers'>Suppliers</Link>
-                                </MenuItem>
-                            </Menu>
-                        </nav>
                     </div>
-                </div>
+                </Paper>
             </header>
         </>
     );
--- client/src/pages/AddForm/AddForm.js
+++ client/src/pages/AddForm/AddForm.js
@@ -10,15 +10,43 @@ import styles from './AddForm.module.css'
 export default function AddForm() {
     return (
         <>
-            <main className={styles.addFormPage}>
-                <Paper>
-                    <Typography>
-
-                    </Typography>
-                    <TextField />
-                    <Button>ads</Button>
-                </Paper>
-            </main>
+            <Paper variant="outlined" className={styles.AddFormContent}>
+                <Typography className={styles.AddFormTitle} variant="h3" component="h3">
+                    Add a position
+                </Typography>
+                <TextField
+                    variant="outlined"
+                    className={styles.field}
+                    label='1'
+                    fullWidth
+                />
+                <TextField
+                    variant="outlined"
+                    className={styles.field}
+                    fullWidth
+                />
+                <TextField
+                    variant="outlined"
+                    className={styles.field}
+                    fullWidth
+                />
+                <TextField
+                    variant="outlined"
+                    className={styles.field}
+                    fullWidth
+                />
+                <TextField
+                    variant="outlined"
+                    className={styles.field}
+                    fullWidth
+                />
+                <TextField
+                    variant="outlined"
+                    className={styles.field}
+                    fullWidth
+                />
+                <Button>Add</Button>
+            </Paper>
         </>
     );
 }
